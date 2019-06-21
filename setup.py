fb3978647fb161622276dd6fde87e3e36a9a439f
Fadi A <fadi.ahmad@chisw.com>
2019-06-21 16:17:51 +0300

    Requirements

--- setup.py
+++ setup.py
@@ -5,20 +5,15 @@ from setuptools import setup, find_packages
 
 WOR_DIR = os.path.dirname(os.path.abspath(__file__))
 
-
-def read_file(f: str, by_line: bool = False) -> Union[str, Generator]:
-    with open(f) as file:
-        if by_line:
-            return (line.strip() for line in file.readlines())
-        return file.read()
-
-
 setup(
     name='container_builder',
     description='Builder for Docker containers',
     version='0.1.2',
     packages=find_packages(),
-    install_requires=read_file(os.path.join(WOR_DIR, 'requirements.txt'), by_line=False),
+    install_requires=(
+        'argh==0.26.2',
+        'docker==4.0.2',
+    ),
     entry_points={
         'console_scripts': [
             'dcb = container_builder.build:main',
